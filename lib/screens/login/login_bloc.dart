fb3d7687ac3205833a62c280a0be8534ba03f9ae
Niranjan1238 <Niranjan1238@github.com>
2023-07-27 12:05:46 +0530

    commit architecture setup

--- lib/screens/login/login_bloc.dart
+++ lib/screens/login/login_bloc.dart
@@ -6,7 +6,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:neopos/utils/utils.dart';
 
 part 'login_event.dart';
-
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
