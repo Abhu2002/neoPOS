f653fe13b8d686367353702725381f46caed0494
omkarneo <omkar.parekh@neosoftmail.com>
2023-07-27 16:53:20 +0530

    Colors Updated

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,21 +3,21 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const primaryColor = Color(0xffec7905);
   static const backgroundColor = Color(0xffffffff);
-  static const primaryColorValue = 0xff074BCF;
+  static const primaryColorValue = 0xffec7905;
 
   static const primarySwatch = MaterialColor(
     AppColors.primaryColorValue,
     <int, Color>{
-      50: Color(0xFFE3F2FD),
-      100: Color(0xFFBBDEFB),
-      200: Color(0xFF90CAF9),
-      300: Color(0xFF64B5F6),
-      400: Color(0xFF42A5F5),
+      50: Color(0xFFFFF3E0),
+      100: Color(0xFFFFE0B2),
+      200: Color(0xFFFFCC80),
+      300: Color(0xFFFFB74D),
+      400: Color(0xFFFFA726),
       500: Color(AppColors.primaryColorValue),
-      600: Color(0xFF1E88E5),
-      700: Color(0xFF1976D2),
-      800: Color(0xFF1565C0),
-      900: Color(0xFF0D47A1),
+      600: Color(0xFFFB8C00),
+      700: Color(0xFFF57C00),
+      800: Color(0xFFEF6C00),
+      900: Color(0xFFE65100),
     },
   );
 }
