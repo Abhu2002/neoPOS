e4b29b494cabd2d2cf0703cd901c819563fd3594
akashkharmale107 <akash.kharmale@neosoftmail.com>
2023-07-28 17:39:16 +0530

    feat NP-21 Dashboard Page

--- lib/screens/dashboard/dashboard_page.dart
+++ lib/screens/dashboard/dashboard_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:neopos/screens/login/login_page.dart';
 import 'package:neopos/utils/app_colors.dart';
 
-
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
 
@@ -15,6 +14,7 @@ class _DashboardPage extends State<DashboardPage> {
   int selectedIndex = 0;
   final SideMenuController sideMenu = SideMenuController();
   PageController pageController = PageController();
+
   @override
   void initState() {
     // Connect SideMenuController and PageController together
@@ -23,50 +23,52 @@ class _DashboardPage extends State<DashboardPage> {
     });
     super.initState();
   }
-  List<SideMenuItem> items = [
-  SideMenuItem(
-        icon: Icon(Icons.category),
-        title: 'Category',
-    onTap: (index, sideMenuController) {
-      sideMenuController.changePage(index);
-    },
-      ),
-      SideMenuItem(
-        icon: Icon(Icons.restaurant_menu),
-        title: 'Products',onTap: (index, sideMenuController) {
-        sideMenuController.changePage(index);
 
+  List<SideMenuItem> items = [
+    SideMenuItem(
+      icon: Icon(Icons.category),
+      title: 'Category',
+      onTap: (index, sideMenuController) {
+        sideMenuController.changePage(index);
       },
-      ),
-      SideMenuItem(
-        icon: Icon(Icons.table_chart),
-        title: 'Tables',
-        onTap: (index, sideMenuController) {
-          sideMenuController.changePage(index);
-        },
-      ),
-      SideMenuItem(
-        icon: Icon(Icons.dashboard),
-        title: 'Dashboard',
-        onTap: (index, sideMenuController) {
-          sideMenuController.changePage(index);
-        },
-      ),
-      SideMenuItem(
-        icon: Icon(Icons.shopping_cart),
-        title: 'Order History',
-        onTap: (index, sideMenuController) {
-          sideMenuController.changePage(index);
-        },
-      ),
-      SideMenuItem(
-        icon: Icon(Icons.person),
-        title: 'Users',
-        onTap: (index, sideMenuController) {
-          sideMenuController.changePage(index);
-        },
-      ),
+    ),
+    SideMenuItem(
+      icon: Icon(Icons.restaurant_menu),
+      title: 'Products',
+      onTap: (index, sideMenuController) {
+        sideMenuController.changePage(index);
+      },
+    ),
+    SideMenuItem(
+      icon: Icon(Icons.table_chart),
+      title: 'Tables',
+      onTap: (index, sideMenuController) {
+        sideMenuController.changePage(index);
+      },
+    ),
+    SideMenuItem(
+      icon: Icon(Icons.dashboard),
+      title: 'Dashboard',
+      onTap: (index, sideMenuController) {
+        sideMenuController.changePage(index);
+      },
+    ),
+    SideMenuItem(
+      icon: Icon(Icons.shopping_cart),
+      title: 'Order History',
+      onTap: (index, sideMenuController) {
+        sideMenuController.changePage(index);
+      },
+    ),
+    SideMenuItem(
+      icon: Icon(Icons.person),
+      title: 'Users',
+      onTap: (index, sideMenuController) {
+        sideMenuController.changePage(index);
+      },
+    ),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,39 +77,50 @@ class _DashboardPage extends State<DashboardPage> {
           actions: [
             IconButton(
                 onPressed: () async {
-                  Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => LoginPage(),)) ;
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginPage(),
+                      ));
                 },
                 icon: const Icon(Icons.logout))
           ],
         ),
         body: SafeArea(
-          child: CustomScrollView(
-            slivers: [
-              SliverFillRemaining(
-                child: Row(mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SideMenu(items: items, controller: sideMenu,style: SideMenuStyle(backgroundColor: Colors.grey.shade50,selectedColor: AppColors.primarySwatch.shade50,selectedIconColor: AppColors.primarySwatch.shade400,selectedTitleTextStyle: TextStyle(color: AppColors.primarySwatch.shade400))),
-                    Expanded(
-                      child: PageView(
-                        controller: pageController,
-                        children: [
-                          Container(
-                            child: Center(
-                              child: Text('Dashboard'),
-                            ),
-                          ),
-                          Container(
-                            child: Center(
-                              child: Text('Settings'),
-                            ),
-                          ),
-                  ],
-                ),
-              )
-            ],
-           ),
-        )])));
+            child: CustomScrollView(slivers: [
+          SliverFillRemaining(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SideMenu(
+                    items: items,
+                    controller: sideMenu,
+                    style: SideMenuStyle(
+                        backgroundColor: Colors.grey.shade50,
+                        selectedColor: AppColors.primarySwatch.shade50,
+                        selectedIconColor: AppColors.primarySwatch.shade400,
+                        selectedTitleTextStyle: TextStyle(
+                            color: AppColors.primarySwatch.shade400))),
+                Expanded(
+                  child: PageView(
+                    controller: pageController,
+                    children: [
+                      Container(
+                        child: Center(
+                          child: Text('Dashboard'),
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text('Settings'),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ])));
   }
 }
-
-
