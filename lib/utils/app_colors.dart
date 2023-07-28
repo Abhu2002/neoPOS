701cbecf40442a0843b66181813f1f9ee14086ab
omkarneo <omkar.parekh@neosoftmail.com>
2023-07-28 10:28:06 +0530

    Feat [Np-18] Comments Added

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+// Theme Primary Color
   static const primaryColor = Color(0xffec7905);
   static const backgroundColor = Color(0xffffffff);
   static const primaryColorValue = 0xffec7905;
-  static const textColorValue = Color(0x00000000);
+
+// Text Colors
+  static const textColor = Color(0x00000000);
   static const mainTextColor = Color(0xffec7905);
-  static const subTextColorValue = Color(0xff2A2A2A);
-  static const buttonTextColorValue = Color(0xffec7905);
+  static const subTextColor = Color(0xff2A2A2A);
+  static const buttonTextColor = Color(0xffec7905);
+
+// Table Colors
   static const tableAvailableColor = Color(0xff55CA92);
   static const tableNotAvailableColor = Color(0xf5EC7904);
   static const tableAvailableTextColor = Color(0xf5EC7904);
   static const tableunAvailableTextColor = Color(0xffEBEBEB);
 
+// Swatch Colors
   static const primarySwatch = MaterialColor(
     AppColors.primaryColorValue,
     <int, Color>{
