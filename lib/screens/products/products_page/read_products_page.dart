e9a160b56304625975787e999be01c26fd845b3f
omkarneo <omkar.parekh@neosoftmail.com>
2023-08-04 15:19:01 +0530

    Bar Added

--- lib/screens/products/products_page/read_products_page.dart
+++ lib/screens/products/products_page/read_products_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:neopos/screens/products/products_page/moreinfo_dialog.dart';
@@ -73,23 +72,6 @@ class _ProductsReadState extends State<ProductsRead> {
                                         color: AppColors.primaryColor),
                                   ))),
                               const SizedBox(width: 20),
-      SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: BlocBuilder<ReadProductsBloc, ReadProductsState>(
-          builder: (context, state) {
-            if (state is ReadDataLoadedState) {
-              return SizedBox(
-                width: MediaQuery.sizeOf(context).width,
-                child: DataTable(
-                  showBottomBorder: true,
-                  headingRowHeight: 60,
-                  // columnSpacing: 2,
-                  headingTextStyle: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.mainTextColor),
-                  // Use the default value.
-
                               Container(
                                   width: 100,
                                   child: const Center(
@@ -148,92 +130,133 @@ class _ProductsReadState extends State<ProductsRead> {
                         );
                       }
 
-                      return Container(
-                        width: 100,
-                        height: 100,
-                        child: Row(children: [
-                          Container(
-                            width: 80,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Center(child: Text("1 Aug")),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 20,
-                          ),
-                          Container(
-                            width: 100,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.network(
-                                "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-                                width: 50,
-                                fit: BoxFit.fill,
+                      return InkWell(
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          child: Row(children: [
+                            Container(
+                              width: 80,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Center(child: Text("1 Aug")),
                               ),
                             ),
-                          ),
-                          const SizedBox(
-                            width: 80,
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  data['product_name'],
-                                  style: const TextStyle(
-                                      fontSize: 15,
-                                      color: AppColors.primaryColor,
-                                      fontWeight: FontWeight.bold),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            Container(
+                              width: 100,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Image.network(
+                                  "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+                                  width: 50,
+                                  fit: BoxFit.fill,
                                 ),
-                                Text(
-                                  data['product_description'].length > 50
-                                      ? data['product_description']
-                                              .substring(0, 50) +
-                                          '...'
-                                      : data['product_description'],
-                                  style: const TextStyle(
-                                    fontSize: 10,
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 40,
-                          ),
-                          Container(
-                            height: 20,
-                            width:
-                                (data['product_type'] == "Non-veg") ? 60 : 30,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(5),
-                                color: (data['product_type']) == "Non-veg"
-                                    ? Colors.red
-                                    : Colors.green),
-                            child: Center(
-                              child: Text(
-                                (data['product_type'] == "Non-veg")
-                                    ? "Non-Veg"
-                                    : "Veg",
-                                style: const TextStyle(color: Colors.white),
                               ),
                             ),
-                          ),
-                          SizedBox(
-                            width:
-                                (data['product_type'] == "Non-veg") ? 30 : 60,
-                          ),
-                          Expanded(
+                            const SizedBox(
+                              width: 80,
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    data['product_name'],
+                                    style: const TextStyle(
+                                        fontSize: 15,
+                                        color: AppColors.primaryColor,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    data['product_description'].length > 50
+                                        ? data['product_description']
+                                                .substring(0, 50) +
+                                            '...'
+                                        : data['product_description'],
+                                    style: const TextStyle(
+                                      fontSize: 10,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 40,
+                            ),
+                            Container(
+                              height: 20,
+                              width:
+                                  (data['product_type'] == "Non-veg") ? 60 : 30,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: (data['product_type']) == "Non-veg"
+                                      ? Colors.red
+                                      : Colors.green),
                               child: Center(
-                                  child: Text(data["product_category"]!))),
-                          Expanded(
-                              child: Center(
-                                  child: Text("Rs ${data["product_price"]!}"))),
-                        ]),
+                                child: Text(
+                                  (data['product_type'] == "Non-veg")
+                                      ? "Non-Veg"
+                                      : "Veg",
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width:
+                                  (data['product_type'] == "Non-veg") ? 30 : 60,
+                            ),
+                            Expanded(
+                                child: Center(
+                                    child: Text(data["product_category"]!))),
+                            Expanded(
+                                child: Center(
+                                    child:
+                                        Text("Rs ${data["product_price"]!}"))),
+                          ]),
+                        ),
+                        onTap: () {
+                          showGeneralDialog(
+                              context: context,
+                              transitionBuilder: (BuildContext context,
+                                  Animation<double> animation,
+                                  Animation<double> secondaryAnimation,
+                                  Widget child) {
+                                return SlideTransition(
+                                  position: Tween(
+                                          begin: const Offset(1, 0),
+                                          end: const Offset(0, 0))
+                                      .animate(animation),
+                                  child: FadeTransition(
+                                    opacity: CurvedAnimation(
+                                      parent: animation,
+                                      curve: Curves.easeOut,
+                                    ),
+                                    child: child,
+                                  ),
+                                );
+                              },
+                              pageBuilder:
+                                  (context, animation, secondaryAnimation) {
+                                return Align(
+                                    alignment: Alignment.centerRight,
+                                    child: MoreInfoPopup(
+                                      image: data['product_image'],
+                                      productName: data["product_name"]!,
+                                      productDescription:
+                                          data["product_description"]!,
+                                      productType: data['product_type'],
+                                      productAvailibility:
+                                          data['product_availability'],
+                                      productPrice: data["product_price"],
+                                      productCategory: data['product_category'],
+                                    ));
+                              });
+                        },
                       );
                     },
                   ),
