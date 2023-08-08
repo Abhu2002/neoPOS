e441306b13407e8fc9bcfefde8c731667f0a4f1d
omkarneo <omkar.parekh@neosoftmail.com>
2023-08-08 12:21:36 +0530

    Commit Solving

--- lib/screens/dashboard/dashboard_page.dart
+++ lib/screens/dashboard/dashboard_page.dart
@@ -4,6 +4,7 @@ import '../../navigation/route_paths.dart';
 import 'package:neopos/utils/app_colors.dart';
 import '../order page/order_page/order_read_page/order_read_page.dart';
 import '../products/products_page/read_products_bloc.dart';
+import '../products/products_page/read_products_page.dart';
 import '../table/table_page/table_page.dart';
 import '../category/category_page/read_category_page.dart';
 import '../users/user_page/read_user_page.dart';
