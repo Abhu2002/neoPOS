f30168ea3f4c32e5303edc9b909af92e9feb8b89
Jabbar Shaikh <jabbarshaikh247@gmail.com>
2023-08-14 10:33:40 +0530

    feat [NP-77] widgets segregated

---
+++ lib/screens/order page/widgets/add_order.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:input_quantity/input_quantity.dart';
+import 'package:neopos/utils/app_colors.dart';
+import 'package:neopos/utils/popup_cancel_button.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../order_menu_page/order_menu_bloc.dart';
+
+
+class AddOrder extends StatefulWidget {
+  const AddOrder(
+      {required this.productName,
+      super.key,
+      required this.productCategory,
+      required this.productType,
+      required this.productPrice,
+      required this.docId});
+
+  final String productName;
+  final String productCategory;
+  final String productType;
+  final String productPrice;
+  final String docId;
+
+  @override
+  State<AddOrder> createState() => _AddOrderState();
+}
+
+class _AddOrderState extends State<AddOrder> {
+  int quantity = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      actionsPadding: const EdgeInsets.all(20),
+      title: PopUpRow(
+        title: AppLocalizations.of(context)!.add_order,
+      ),
+      actions: [
+        SizedBox(
+          width: 300,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                widget.productName,
+                softWrap: true,
+                style: const TextStyle(
+                  fontSize: 22,
+                  color: AppColors.mainTextColor,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  const Expanded(
+                      flex: 1,
+                      child: Text("QTY : ",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: AppColors.mainTextColor,
+                          ))),
+                  Expanded(
+                    flex: 5,
+                    child: InputQty(
+                      maxVal: 10,
+                      initVal: 1,
+                      minVal: 1,
+                      isIntrinsicWidth: false,
+                      borderShape: BorderShapeBtn.circle,
+                      boxDecoration: const BoxDecoration(),
+                      steps: 1,
+                      showMessageLimit: false,
+                      onQtyChanged: (val) {
+                        quantity = val!.toInt();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.primaryColor),
+                onPressed: () {
+                  BlocProvider.of<OrderContentBloc>(context).add(
+                      AddOrderFBEvent(
+                          widget.productName,
+                          widget.productType,
+                          widget.productCategory,
+                          widget.productPrice,
+                          quantity.toString(),
+                          widget.docId));
+                  Navigator.pop(context);
+                },
+                child: Text(AppLocalizations.of(context)!.add_button),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/screens/order page/widgets/menu_btns_widget.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../order_menu_page/order_menu_bloc.dart';
+
+class MenuBtnsWidget extends StatefulWidget {
+  const MenuBtnsWidget({super.key});
+
+  @override
+  State<MenuBtnsWidget> createState() => _MenuBtnsWidgetState();
+}
+
+class _MenuBtnsWidgetState extends State<MenuBtnsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text("Menu",
+                style: TextStyle(
+                    fontSize: 20, fontWeight: FontWeight.bold)),
+          ),
+          BlocBuilder<OrderContentBloc, OrderContentState>(
+            builder: (context, state) {
+              if (state is ProductLoadingState ||
+                  state is FilterProductsState) {
+                List<String> allCats = state.allCats;
+                List<Widget> catBtns = [];
+
+                catBtns.add(
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child: ActionChip(
+                      elevation: 8.0,
+                      padding: const EdgeInsets.all(2.0),
+                      avatar: const CircleAvatar(
+                        backgroundColor: Colors.orangeAccent,
+                        child: Icon(
+                          Icons.food_bank_outlined,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                      label: const Text("All"),
+                      onPressed: () {
+                        BlocProvider.of<OrderContentBloc>(context)
+                            .add(FilterProductsEvent("All",
+                            state.allProds, state.allCats));
+                      },
+                      backgroundColor: (state.category == "All" ||
+                          state.category == "")
+                          ? Colors.orangeAccent
+                          : Colors.white,
+                      shape: const StadiumBorder(
+                          side: BorderSide(
+                            width: 1,
+                            color: Colors.orange,
+                          )),
+                    ),
+                  ),
+                );
+                for (var element in allCats) {
+                  catBtns.add(
+                    Container
+                      (
+                      margin: const EdgeInsets.all(10),
+                      child: ActionChip(
+                        elevation: 8.0,
+                        padding: const EdgeInsets.all(2.0),
+                        avatar: const CircleAvatar(
+                          backgroundColor: Colors.orangeAccent,
+                          child: Icon(
+                            Icons.rice_bowl,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                        label: Text(element),
+                        onPressed: () {
+                          BlocProvider.of<OrderContentBloc>(
+                              context)
+                              .add(FilterProductsEvent(element,
+                              state.allProds, state.allCats));
+                        },
+                        backgroundColor: (state.category == element)
+                            ? Colors.orangeAccent
+                            : Colors.white,
+                        shape: const StadiumBorder(
+                            side: BorderSide(
+                              width: 1,
+                              color: Colors.orange,
+                            )),
+                      ),
+                    ),
+                  );
+                }
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: catBtns,
+                  ),
+                );
+              }
+              return Spacer();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
