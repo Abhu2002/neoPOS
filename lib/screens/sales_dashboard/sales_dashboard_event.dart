5e119a914a25c88d33a3412d3f5c5d4504721edb
omkarneo <omkar.parekh@neosoftmail.com>
2023-08-16 10:57:50 +0530

    DashBoard folder Structure Added

---
+++ lib/screens/sales_dashboard/sales_dashboard_event.dart
@@ -0,0 +1,15 @@
+part of 'sales_dashboard_bloc.dart';
+
+abstract class SalesDashboardEvent extends Equatable {
+  const SalesDashboardEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class DashboardPageinitevent extends SalesDashboardEvent {
+  const DashboardPageinitevent();
+
+  @override
+  List<Object> get props => [];
+}
