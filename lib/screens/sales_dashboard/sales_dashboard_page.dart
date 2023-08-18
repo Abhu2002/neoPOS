7a4867c46c0e9f25a3f6f3505441d667777d407a
akashkharmale107 <akash.kharmale@neosoftmail.com>
2023-08-18 17:28:14 +0530

    NP-101 Dashboard According to user Role and Graph Bug

--- lib/screens/sales_dashboard/sales_dashboard_page.dart
+++ lib/screens/sales_dashboard/sales_dashboard_page.dart
@@ -19,27 +19,12 @@ class SalesDashboardPage extends StatefulWidget {
   State<SalesDashboardPage> createState() => _SalesDashboardPageState();
 }
 
-enum MonthLabel {
-  January,
-  February,
-  March,
-  April,
-  May,
-  June,
-  July,
-  August,
-  Septeber,
-  October,
-  November,
-  December
-}
 
 class _SalesDashboardPageState extends State<SalesDashboardPage> {
   MonthLabel? selectedMonth;
 
   @override
   void initState() {
-    selectedMonth = isCurrentMonth(DateTime.now().month);
     super.initState();
     BlocProvider.of<SalesDashboardBloc>(
       context,
@@ -49,11 +34,11 @@ class _SalesDashboardPageState extends State<SalesDashboardPage> {
   final TextEditingController monthController = TextEditingController();
 
   var f = NumberFormat("###,###", "en_US");
-  var senddata1;
+
   @override
   Widget build(BuildContext context) {
     final List<DropdownMenuEntry<MonthLabel>> monthEntries =
-        <DropdownMenuEntry<MonthLabel>>[];
+    <DropdownMenuEntry<MonthLabel>>[];
     for (final MonthLabel month in MonthLabel.values) {
       monthEntries.add(
         DropdownMenuEntry<MonthLabel>(value: month, label: month.name),
@@ -62,233 +47,174 @@ class _SalesDashboardPageState extends State<SalesDashboardPage> {
     return Column(children: [
       SizedBox(
           child: Column(children: [
-        Container(
-          color: Colors.grey.shade100,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 380,
-                width: MediaQuery.sizeOf(context).width,
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: BlocBuilder<SalesDashboardBloc,
-                            SalesDashboardState>(
-                          builder: (context, state) {
-                            if (state is SalesDashBoardLoadedState) {
-                              var sendData = state.allOrder
-                                  .sublist(state.allOrder.length - 5,
+            Container(
+              color: Colors.grey.shade100,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 380,
+                    width: MediaQuery
+                        .sizeOf(context)
+                        .width,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: BlocBuilder<SalesDashboardBloc,
+                                SalesDashboardState>(
+                              builder: (context, state) {
+                                if (state is SalesDashBoardLoadedState) {
+                                  var sendData = state.allOrder
+                                      .sublist(state.allOrder.length - 5,
                                       state.allOrder.length)
-                                  .toList();
-                              senddata1 = sendData;
-                              return Card(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Column(
-                                  children: [
-                                    ListTile(
-                                        title: const Text(
-                                          "Last 5 Orders Transaction",
-                                          style: TextStyle(
-                                              fontSize: 15,
-                                              fontWeight: FontWeight.bold,
-                                              color: AppColors.primaryColor),
-                                        ),
-                                        trailing: TextButton(
-                                          child: const Text("All Order"),
-                                          onPressed: () {
-                                            if (widget.controller.hasClients) {
-                                              widget.controller.animateToPage(
-                                                5,
-                                                duration: const Duration(
-                                                    milliseconds: 400),
-                                                curve: Curves.easeInOut,
-                                              );
-                                              widget.sidemenu.changePage(5);
-                                            }
-                                          },
-                                        )),
-                                    ListView.builder(
-                                      reverse: true,
-                                      shrinkWrap: true,
-                                      itemCount: 5,
-                                      itemBuilder: (context, index) {
-                                        var data = senddata1[index];
-                                        return ListTile(
-                                          tileColor: Colors.white,
-                                          title: Text(
-                                            "Order Id ${data['Id']}",
-                                          ),
-                                          subtitle: Text(
-                                            "₹${f.format(data["amount"])}",
-                                          ),
-                                          trailing: Text(
-                                            DateFormat('MMM dd hh:mm').format(
-                                                DateTime.parse(
-                                                    data['order_date'])),
-                                          ),
-                                          leading: const Icon(
-                                            Icons.history_toggle_off,
-                                            color: AppColors.primaryColor,
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              );
-                            } else if (state is SalesDashBoardLoadingState) {
-                              return Container();
-                            }
-                            return const Text("");
-                          },
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                        flex: 3,
-                        child: BlocBuilder<SalesDashboardBloc,
-                            SalesDashboardState>(
-                          builder: (context, state) {
-                            if (state is SalesDashBoardLoadedState) {
-                              return Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      Expanded(
-                                          child: CommonCard(
-                                        title: "Today's Revenue",
-                                        amount: state.dailyValue.toString(),
-                                      )),
-                                      Expanded(
-                                          child: CommonCard(
-                                        title: "Weekly Revenue",
-                                        amount: state.weeklyValue.toString(),
-                                      )),
-                                      Expanded(
-                                          child: CommonCard(
-                                        title: "Monthly Revenue",
-                                        amount: state.monthlyValue.toString(),
-                                      )),
-                                    ],
-                                  ),
-                                  Card(
+                                      .toList();
+                                  return Card(
                                     shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10)),
-                                    child: SizedBox(
-                                      height: 268,
-                                      child: PieChart(
-                                        dataMap: state.piemap,
-                                        // chartType: ChartType.ring,
-                                        baseChartColor:
-                                            Colors.grey[50]!.withOpacity(0.15),
-                                        chartValuesOptions:
-                                            const ChartValuesOptions(
-                                          showChartValuesInPercentage: true,
+                                        borderRadius: BorderRadius.circular(
+                                            10)),
+                                    child: Column(
+                                      children: [
+                                        ListTile(
+                                            title: const Text(
+                                              "Last 5 Orders Transaction",
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: AppColors
+                                                      .primaryColor),
+                                            ),
+                                            trailing: TextButton(
+                                              child: const Text("All Order"),
+                                              onPressed: () {
+                                                if (widget.controller
+                                                    .hasClients) {
+                                                  widget.controller
+                                                      .animateToPage(
+                                                    5,
+                                                    duration: const Duration(
+                                                        milliseconds: 400),
+                                                    curve: Curves.easeInOut,
+                                                  );
+                                                  widget.sidemenu.changePage(5);
+                                                }
+                                              },
+                                            )),
+                                        ListView.builder(
+                                          reverse: true,
+                                          shrinkWrap: true,
+                                          itemCount: 5,
+                                          itemBuilder: (context, index) {
+                                            var data = sendData[index];
+                                            return ListTile(
+                                              tileColor: Colors.white,
+                                              title: Text(
+                                                "Order Id ${data['Id']}",
+                                              ),
+                                              subtitle: Text(
+                                                "₹${f.format(data["amount"])}",
+                                              ),
+                                              trailing: Text(
+                                                DateFormat('MMM dd hh:mm')
+                                                    .format(
+                                                    DateTime.parse(
+                                                        data['order_date'])),
+                                              ),
+                                              leading: const Icon(
+                                                Icons.history_toggle_off,
+                                                color: AppColors.primaryColor,
+                                              ),
+                                            );
+                                          },
                                         ),
-                                        // totalValue: 20,
-                                      ),
+                                      ],
                                     ),
-                                  ),
-                                ],
-                              );
-                            } else {
-                              return Container();
-                            }
-                          },
-                        )),
+                                  );
+                                } else
+                                if (state is SalesDashBoardLoadingState) {
+                                  return Container();
+                                }
+                                return const Text("");
+                              },
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                            flex: 3,
+                            child: BlocBuilder<SalesDashboardBloc,
+                                SalesDashboardState>(
+                              builder: (context, state) {
+                                if (state is SalesDashBoardLoadedState) {
+                                  return Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Expanded(
+                                              child: CommonCard(
+                                                title: "Today's Revenue",
+                                                amount: state.dailyValue
+                                                    .toString(),
+                                              )),
+                                          Expanded(
+                                              child: CommonCard(
+                                                title: "Weekly Revenue",
+                                                amount: state.weeklyValue
+                                                    .toString(),
+                                              )),
+                                          Expanded(
+                                              child: CommonCard(
+                                                title: "Monthly Revenue",
+                                                amount: state.monthlyValue
+                                                    .toString(),
+                                              )),
+                                        ],
+                                      ),
+                                      Card(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                            BorderRadius.circular(10)),
+                                        child: SizedBox(
+                                          height: 268,
+                                          child: PieChart(
+                                            dataMap: state.piemap,
+                                            // chartType: ChartType.ring,
+                                            baseChartColor:
+                                            Colors.grey[50]!.withOpacity(0.15),
+                                            chartValuesOptions:
+                                            const ChartValuesOptions(
+                                              showChartValuesInPercentage: true,
+                                            ),
+                                            // totalValue: 20,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                } else {
+                                  return Container();
+                                }
+                              },
+                            )),
 
-                    /// TODO Need Top sales to be Added Later
-                    // Expanded(
-                    //     flex: 2,
-                    //     child: Card(
-                    //       shape: RoundedRectangleBorder(
-                    //           borderRadius: BorderRadius.circular(10)),
-                    //       child: Container(
-                    //         height: 380,
-                    //         child: const Center(child: Text("Top Sales")),
-                    //       ),
-                    //     ))
-                  ],
-                ),
+                        /// TODO Need Top sales to be Added Later
+                        // Expanded(
+                        //     flex: 2,
+                        //     child: Card(
+                        //       shape: RoundedRectangleBorder(
+                        //           borderRadius: BorderRadius.circular(10)),
+                        //       child: Container(
+                        //         height: 380,
+                        //         child: const Center(child: Text("Top Sales")),
+                        //       ),
+                        //     ))
+                      ],
+                    ),
+                  ),
+                  const GraphPage()
+                ],
               ),
-              const GraphPage()
-            ],
-          ),
-        )
-      ]))
+            )
+          ]))
     ]);
   }
-
-  int isMonth(MonthLabel? a) {
-    switch (a) {
-      case MonthLabel.January:
-        return 1;
-      case MonthLabel.February:
-        return 2;
-      case MonthLabel.March:
-        return 3;
-      case MonthLabel.April:
-        return 4;
-      case MonthLabel.May:
-        return 5;
-      case MonthLabel.June:
-        return 6;
-      case MonthLabel.July:
-        return 7;
-      case MonthLabel.August:
-        return 8;
-      case MonthLabel.Septeber:
-        return 9;
-      case MonthLabel.October:
-        return 10;
-      case MonthLabel.November:
-        return 11;
-      case MonthLabel.December:
-        return 12;
-      default:
-        return 1;
-    }
-  }
-
-  MonthLabel isCurrentMonth(int a) {
-    switch (a) {
-      case 1:
-        return MonthLabel.January;
-      case 2:
-        return MonthLabel.February;
-      case 3:
-        return MonthLabel.March;
-      case 4:
-        return MonthLabel.April;
-      case 5:
-        return MonthLabel.May;
-      case 6:
-        return MonthLabel.June;
-      case 7:
-        return MonthLabel.July;
-      case 8:
-        return MonthLabel.August;
-      case 9:
-        return MonthLabel.Septeber;
-      case 10:
-        return MonthLabel.October;
-      case 11:
-        return MonthLabel.November;
-      case 12:
-        return MonthLabel.December;
-      default:
-        return MonthLabel.December;
-    }
-  }
-}
-
-class ChartData {
-  ChartData(this.x, this.y);
-
-  final int x;
-  final double y;
 }
