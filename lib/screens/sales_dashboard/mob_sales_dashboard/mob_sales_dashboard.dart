237d2a539bfcd3d95b03258ce39070df9a082c84
akashkharmale107 <akash.kharmale@neosoftmail.com>
2023-08-25 14:35:10 +0530

    commit

--- lib/screens/sales_dashboard/mob_sales_dashboard/mob_sales_dashboard.dart
+++ lib/screens/sales_dashboard/mob_sales_dashboard/mob_sales_dashboard.dart
@@ -57,13 +57,17 @@ class _MobileSalesDashboardPageState extends State<MobileSalesDashboardPage> {
         child: Column(
           children: [
             SizedBox(
-              height: MediaQuery.sizeOf(context).height,
+             // height: MediaQuery.sizeOf(context).height,
               width: MediaQuery.sizeOf(context).width,
               child: Column(
                 children: [
                   BlocBuilder<SalesDashboardBloc, SalesDashboardState>(
                     builder: (context, state) {
                       if (state is SalesDashBoardLoadedState) {
+                        var sendData = state.allOrder
+                            .sublist(state.allOrder.length - 5,
+                                state.allOrder.length)
+                            .toList();
                         List<Widget> cards = [
                           CommonCardMobile(
                               title:
@@ -78,40 +82,134 @@ class _MobileSalesDashboardPageState extends State<MobileSalesDashboardPage> {
                                   AppLocalizations.of(context)!.monthly_revenue,
                               amount: state.monthlyValue.toString())
                         ];
-                        return Column(
-                          children: [
-                            SizedBox(
-                              height: 150,
-                              width: MediaQuery.of(context).size.width,
-                              child: CarouselSlider.builder(
-                                //carouselController: carouselController.,
-                                itemCount: 3,
-                                itemBuilder: (BuildContext context, int index,
-                                    int realIndex) {
-                                  return cards[index];
-                                },
-                                options: CarouselOptions(
-                                    enableInfiniteScroll: true,
-                                    viewportFraction: 0.6),
+                        List<Widget> data = [
+                          Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            child: SizedBox(
+                              height: 450,
+                              child: PieChart(
+                                legendOptions: const LegendOptions(
+                                    showLegendsInRow: true,
+                                    legendPosition: LegendPosition.bottom),
+                                dataMap: state.piemap,
+                                // chartType: ChartType.ring,
+                                baseChartColor:
+                                    Colors.grey[50]!.withOpacity(0.15),
+                                chartValuesOptions: const ChartValuesOptions(
+                                  showChartValuesInPercentage: true,
+                                ),
+                                // totalValue: 20,
                               ),
                             ),
-                            Card(
+                          ),
+                          SizedBox(
+                            height: 450,
+                            child: Card(
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(10)),
-                              child: SizedBox(
-                                height: 400,
-                                child: PieChart(
-                                  legendOptions: const LegendOptions(
-                                      showLegendsInRow: true,
-                                      legendPosition: LegendPosition.bottom),
-                                  dataMap: state.piemap,
-                                  // chartType: ChartType.ring,
-                                  baseChartColor:
-                                      Colors.grey[50]!.withOpacity(0.15),
-                                  chartValuesOptions: const ChartValuesOptions(
-                                    showChartValuesInPercentage: true,
+                              child: Column(
+                                children: [
+                                  ListTile(
+                                      title: Text(
+                                        AppLocalizations.of(context)!
+                                            .sales_dash_order,
+                                        style: const TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                            color: AppColors.primaryColor),
+                                      ),
+                                      trailing: TextButton(
+                                        child: Text(
+                                            AppLocalizations.of(context)!
+                                                .sales_dash_more),
+                                        onPressed: () {
+                                          if (widget
+                                              .pageController.hasClients) {
+                                            widget.pageController.animateToPage(
+                                              5,
+                                              duration: const Duration(
+                                                  milliseconds: 400),
+                                              curve: Curves.easeInOut,
+                                            );
+                                            widget.pageController.jumpToPage(5);
+                                            BlocProvider.of<SideMenuBloc>(
+                                                    context)
+                                                .add(SideMenuInitEvent(5));
+                                          }
+                                        },
+                                      )),
+                                  ListView.builder(
+                                    physics:
+                                        const NeverScrollableScrollPhysics(),
+                                    reverse: true,
+                                    shrinkWrap: true,
+                                    itemCount: 5,
+                                    itemBuilder: (context, index) {
+                                      var data = sendData[index];
+                                      return ListTile(
+                                        tileColor: Colors.white,
+                                        title: Text(
+                                          "Order Id ${data['Id']}",
+                                        ),
+                                        subtitle: Text(
+                                          "₹${f.format(data["amount"])}",
+                                        ),
+                                        trailing: Text(
+                                          DateFormat('MMM dd hh:mm').format(
+                                              DateTime.parse(
+                                                  data['order_date'])),
+                                        ),
+                                        leading: const Icon(
+                                          Icons.history_toggle_off,
+                                          color: AppColors.primaryColor,
+                                        ),
+                                      );
+                                    },
                                   ),
-                                  // totalValue: 20,
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                              height: 450,
+                              child: TopFiveProduct(data: state.topproduct))
+                        ];
+                        return Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: SizedBox(
+                                height: 150,
+                                width: MediaQuery.of(context).size.width,
+                                child: CarouselSlider.builder(
+                                  //carouselController: carouselController.,
+                                  itemCount: 3,
+                                  itemBuilder: (BuildContext context, int index,
+                                      int realIndex) {
+                                    return cards[index];
+                                  },
+                                  options: CarouselOptions(
+                                      enableInfiniteScroll: true,
+                                      viewportFraction: 0.6),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: CarouselSlider.builder(
+                                  //carouselController: carouselController.,
+                                  itemCount: 3,
+                                  itemBuilder: (BuildContext context, int index,
+                                      int realIndex) {
+                                    return Center(child: data[index]);
+                                  },
+                                  options: CarouselOptions(
+                                    height: 450,
+                                    enableInfiniteScroll: true,
+                                  ),
                                 ),
                               ),
                             ),
@@ -125,112 +223,10 @@ class _MobileSalesDashboardPageState extends State<MobileSalesDashboardPage> {
                       }
                     },
                   ),
-                  Expanded(
-                    child: SizedBox(
-                      height: 300,
-                      child: SingleChildScrollView(
-                        child: BlocBuilder<SalesDashboardBloc,
-                            SalesDashboardState>(
-                          builder: (context, state) {
-                            if (state is SalesDashBoardLoadedState) {
-                              var sendData = state.allOrder
-                                  .sublist(state.allOrder.length - 5,
-                                      state.allOrder.length)
-                                  .toList();
-                              return Card(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Column(
-                                  children: [
-                                    ListTile(
-                                        title: Text(
-                                          AppLocalizations.of(context)!
-                                              .sales_dash_order,
-                                          style: const TextStyle(
-                                              fontSize: 15,
-                                              fontWeight: FontWeight.bold,
-                                              color: AppColors.primaryColor),
-                                        ),
-                                        trailing: TextButton(
-                                          child: Text(
-                                              AppLocalizations.of(context)!
-                                                  .sales_dash_more),
-                                          onPressed: () {
-                                            if (widget
-                                                .pageController.hasClients) {
-                                              widget.pageController
-                                                  .animateToPage(
-                                                5,
-                                                duration: const Duration(
-                                                    milliseconds: 400),
-                                                curve: Curves.easeInOut,
-                                              );
-                                              widget.pageController
-                                                  .jumpToPage(5);
-                                              BlocProvider.of<SideMenuBloc>(
-                                                      context)
-                                                  .add(SideMenuInitEvent(5));
-                                            }
-                                          },
-                                        )),
-                                    ListView.builder(
-                                      physics:
-                                          const NeverScrollableScrollPhysics(),
-                                      reverse: true,
-                                      shrinkWrap: true,
-                                      itemCount: 5,
-                                      itemBuilder: (context, index) {
-                                        var data = sendData[index];
-                                        return ListTile(
-                                          tileColor: Colors.white,
-                                          title: Text(
-                                            "Order Id ${data['Id']}",
-                                          ),
-                                          subtitle: Text(
-                                            "₹${f.format(data["amount"])}",
-                                          ),
-                                          trailing: Text(
-                                            DateFormat('MMM dd hh:mm').format(
-                                                DateTime.parse(
-                                                    data['order_date'])),
-                                          ),
-                                          leading: const Icon(
-                                            Icons.history_toggle_off,
-                                            color: AppColors.primaryColor,
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              );
-                            } else if (state is SalesDashBoardLoadingState) {
-                              return const SizedBox(
-                                  height: 200,
-                                  width: 200,
-                                  child: Center(
-                                      child: CircularProgressIndicator()));
-                            }
-                            return const Text("error");
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
                 ],
               ),
             ),
-            const GraphPage(),
-            SizedBox(height: 280,
-              child: BlocBuilder<SalesDashboardBloc, SalesDashboardState>(
-                  builder: (context, state) {
-                    if (state is SalesDashBoardLoadedState) {
-                      return TopFiveProduct(data: state.topproduct);
-                    } else {
-                      return CircularProgressIndicator();
-                    }
-                  }),
-            ),
+            const GraphPage()
           ],
         ),
       ),
